cbcfdebae4f20edee34aecc6d2adc08f78874b7e
faisal <alifalaksher3636@gmail.com>
2022-03-26 20:45:19 +0500

    Main JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strick";
+// const color = document.querySelector(".git").classList.add('hidden');
+// color.addEventListener('click', function(e){
+// //  e.classlist.add('hidden')
+//  color.style.opacity = 0;
+//  console.log(color)
+// })
+
+const color = document.querySelector(".git");
+
+// color.classList.add('hidden')
+color.addEventListener("mouseover", function (e) {
+  const l = color.classList.add("hidden");
+  console.log(l);
+});
